371ca13a72e3fe47371c6df6f368c94c65530098
flixoflax <flixoflax@gmx.de>
2022-09-17 23:59:31 +0200

    Start createProject method

---
+++ src/utils/create-project.ts
@@ -0,0 +1,8 @@
+import { Options } from '../types'
+
+export const createProject = async (
+  projectName: string,
+  options: Options
+): Promise<void> => {
+  // TODO: Implement createProject method
+}
